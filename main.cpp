8e111ae68639a77dd0086a69766efa1e201be2af
LORD PRITHWISH <prithwishchakraborty05@gmail.com>
2024-11-17 15:23:07 +0530

    stable

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 #include<math.h>
 #include<vector>
 // #include<sd
@@ -16,6 +17,17 @@ SDL_Texture* BIRD_SPRITE = NULL;
 SDL_Texture* OBSTICLE = NULL;
 SDL_Texture* REVOBSTICLE = NULL;
 SDL_Texture* BACKGROUND = NULL;
+TTF_Font* font = NULL;
+SDL_Texture* PAUS = NULL;
+SDL_Texture* STRT = NULL;
+SDL_Texture* GOV = NULL;
+SDL_Rect pausrect ;
+SDL_Texture* score = NULL;
+SDL_Texture* val = NULL;
+
+// SDL_Rect pausrect = {300,200,500,700};
+
+
 // int spc = 50;
 
 
@@ -35,6 +47,10 @@ bool init()    {
         return false;
     }
 
+    if ( TTF_Init() < 0 ) {
+	    std::cout << "Error initializing SDL_ttf: " << TTF_GetError() << std::endl;
+    }
+
 
     return true;
 }
@@ -45,6 +61,18 @@ SDL_Texture* loadTexture(const std::string& path) {
         std::cout << "Failed to load texture! IMG_Error: " << IMG_GetError() << std::endl;
     }
     return newTexture;
+}
+
+
+SDL_Texture* textTexture(const std::string& text,SDL_Color color = {0,0,0}) {
+    SDL_Surface* textsurf = TTF_RenderText_Solid( font, text.c_str(), color );
+    if ( !textsurf ) {
+        std::cout << "Failed to render text: " << TTF_GetError() << std::endl;
+    }
+
+    SDL_Texture* texTure = SDL_CreateTextureFromSurface( rend, textsurf );
+
+    return texTure;
 }
 
 bool loader(){
@@ -71,7 +99,47 @@ bool loader(){
         std::cout << "Failed to load REVOBSTICLE texture!" << std::endl;
         return false;
     }
+    BACKGROUND = loadTexture("../assets/flapbg.png");
+    if (!BACKGROUND) {
+        std::cout << "Failed to load BACKGROUND texture!" << std::endl;
+        return false;
+    }
+    font = TTF_OpenFont("../fonts/sifi_font.ttf", 72);
+	if ( !font ) {
+		std::cout << "Error loading font: " << TTF_GetError() << std::endl;
+		return false;
+	}
+    PAUS = textTexture("PAUSED",{155,120,50});
+    if( !PAUS){
+        std::cout << "Error loading text " << TTF_GetError() << std::endl;
+		return false;
+    }
+    pausrect = {static_cast<int>(round(width/2-250)), static_cast<int>(round(height/2-100)), 500, 200};
+    STRT = textTexture("START",{255,20,50});
+    if( !STRT){
+        std::cout << "Error loading text " << TTF_GetError() << std::endl;
+        return false;
+    }
+    GOV = textTexture("GAME OVER",{255,20,50}); 
+    if( !GOV){
+        std::cout << "Error loading text " << TTF_GetError() << std::endl;
+        return false;
+    }
+    score = textTexture("SCORE: ",{20,200,250});
+    if( !score){
+        std::cout << "Error loading text " << TTF_GetError() << std::endl;
+        return false;
+    }
+    val = textTexture("0",{20,200,250});
+    if( !val){
+        std::cout << "Error loading text " << TTF_GetError() << std::endl;
+        return false;
+    }
+
+
     return true;
+
+
 }
 
 std::vector<int> pipe_gen(){
@@ -83,7 +151,32 @@ std::vector<int> pipe_gen(){
     return res;
 }
 
+void kill(){
 
+    if (BIRD != NULL) 
+        SDL_DestroyTexture(BIRD);
+    if (OBSTICLE != NULL) 
+        SDL_DestroyTexture(OBSTICLE);
+    if (REVOBSTICLE != NULL) 
+        SDL_DestroyTexture(REVOBSTICLE);
+    if (BIRD_SPRITE != NULL)
+        SDL_DestroyTexture(BIRD_SPRITE);
+    if (BACKGROUND != NULL)
+        SDL_DestroyTexture(BACKGROUND);
+    if (font != NULL)
+        TTF_CloseFont(font);
+    if (PAUS != NULL)
+        SDL_DestroyTexture(PAUS);
+        
+
+
+
+    SDL_DestroyRenderer(rend);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+    IMG_Quit();
+    TTF_Quit();
+}
 
 
 
@@ -99,13 +192,25 @@ int main(int argc, char* argv[])    {
     }
 
 
+    SDL_Rect pacdim = {static_cast<int>(round(width/6)), static_cast<int>(round(height/4)), pacw, pach};
+    SDL_Rect assinbrd = {20,30,155,138};//EXACT DIMENSIONS OF THE BIRD
 
-    SDL_Rect pacdim = {static_cast<int>(round(width/6)), static_cast<int>(round(height/2)), pacw, pach};
+    SDL_Rect strtrect = {static_cast<int>(round(width/2-250)), static_cast<int>(round(height/2-100)), 500, 200};
+    SDL_Rect govrect = {static_cast<int>(round(width/2-250)), static_cast<int>(round(height/2-100)), 500, 200};
+    SDL_Rect logorect = {10,10,40,40};
+    SDL_Rect scorrect = {80,10,200,40};
+    SDL_Rect valrect = {410,5,50,50};
 
-    bool running = true,paused = true;
+
+    int frame = 0,duration = 0;
+    bool running = true,paused = true,started = false,collided = false;
     uint spat = 0,piptim=0;
     std::vector<std::vector<int>> pipes;
     std::vector<int> front;
+
+    std::vector<SDL_Rect> bg = {{0,0,width,height},{width,0,width,height}};
+    SDL_SetRenderDrawBlendMode(rend, SDL_BLENDMODE_BLEND); // Use blend mode for alpha transparency
+
 
     while(running)    {
         Uint32 startTick = SDL_GetTicks();
@@ -115,30 +220,77 @@ int main(int argc, char* argv[])    {
             case SDL_QUIT: running = false;
                 break;
             case SDL_MOUSEBUTTONDOWN : 
-            if(paused)
+            if(!started)
+                started = true;
+            if(collided){
+                pacdim.y = height/4;
+                pipes.clear();
+                spat = 0;
+                piptim = 0;
+                collided = false;
+                val = textTexture("0",{255,20,50});
+                started = false;
+            }
+            else if(paused)
                 paused = false;
-            if(spat <= (spc/4*3))
-                spat=spc;    
+            else if(spat <= (spc/4*3))
+                spat=spc;
+
             break;
             case SDL_KEYDOWN:
                 if(ev.key.keysym.sym == SDLK_SPACE){
-                    if(paused)
+                    if(!started)
+                        started = true;
+                    if(collided){
+                        pacdim.y = height/4;
+                        pipes.clear();
+                        spat = 0;
+                        piptim = 0;
+                        collided = false;
+                        val = textTexture("0",{255,20,50});
+                        started = false;
+                    }
+                    else if(paused)
                         paused = false;
-                    if(spat <= (spc/4*3))
+                    else if(spat <= (spc/4*3))
                         spat=spc;
+
+                    
                 }
                 if(ev.key.keysym.sym == SDLK_ESCAPE){
                     running = false;
                 }
                 if(ev.key.keysym.sym == SDLK_RETURN){
-                    paused = !paused;
+                    if(collided){
+                        pacdim.y = height/4;
+                        pipes.clear();
+                        spat = 0;
+                        piptim = 0;
+                        collided = false;
+                        val = textTexture("0",{255,20,50});
+                        started = false;
+                    }
+                    else    {
+                        paused = !paused;
+                        if(!started)
+                            started = true;
+                    }
                 }
                 break;
             }
         }
-
-        SDL_SetRenderDrawColor(rend,60,170,255,255);
-        SDL_RenderClear(rend);
+        for(int i=0;i<bg.size();i++){
+            if(!paused)
+                bg[i].x-=1;
+            if(bg[i].x<=-width)
+                bg[i].x = width;
+            SDL_RenderCopy(rend, BACKGROUND, NULL, &bg[i]);
+        }
+        SDL_SetRenderDrawColor(rend, 0, 0, 0, 100);
+        SDL_Rect overlay = {0, 0, width, height};
+        SDL_RenderFillRect(rend, &overlay);
+        // SDL_SetRenderDrawColor(rend,60,170,255,255);
+        // SDL_RenderClear(rend);
         if(!paused){
 
             const uint8_t* press = SDL_GetKeyboardState(NULL);
@@ -150,8 +302,6 @@ int main(int argc, char* argv[])    {
             if(spat){
                 spat--;
                 pacdim.y-=spat/7*2;
-                // pacdim.y-=spat;
-
             }
 
             if(pacdim.y >= (height-(pach/4)))
@@ -162,7 +312,6 @@ int main(int argc, char* argv[])    {
                 pipes.insert(pipes.begin(),pipe_gen());
                 piptim = 0;
             }
-            // std::cout<<pipes.size()<<" @ ";
             SDL_Rect rrr;
             SDL_SetRenderDrawColor(rend,0,200,20,255);
             for(int i=0;i<pipes.size();i++){
@@ -184,10 +333,6 @@ int main(int argc, char* argv[])    {
 
                 if(i>pipes.size()-3){
                     if(((pacdim.x+pacdim.w)>=rrr.x) && ((pacdim.x)<=(rrr.x+rrr.w))){
-                        if(press[SDL_SCANCODE_SPACE]){
-                            std::cout<<"pressed"<<"\n";
-                            std::cout<<pacdim.y<<' '<<pipes[i][1]<<' '<<pacdim.h<<' '<<rrr.y<<'\n';
-                        }
                         SDL_SetRenderDrawColor(rend,200,200,220,255);
                         SDL_RenderDrawLine(rend,220,0,220,height);
                         SDL_RenderDrawLine(rend,270,0,270,height);
@@ -196,11 +341,10 @@ int main(int argc, char* argv[])    {
                             SDL_SetRenderDrawColor(rend,250,0,0,255);
                             SDL_RenderDrawLine(rend,0,pacdim.y,width,pacdim.y);
                             SDL_RenderDrawLine(rend,0,pacdim.y+50,width,pacdim.y+50);
-                            // std::cout<<"collided"<<"\n";
-                            // running = false;
+                            collided = true;
+                            paused = true;
                         }
                         SDL_SetRenderDrawColor(rend,0,200,20,255);
-
                     }
                 }
             }
@@ -211,10 +355,51 @@ int main(int argc, char* argv[])    {
 
             piptim++;
             pacdim.y+= 6;
-            // std::cout<<piptim<<'\n';
 
         }
-        SDL_RenderCopy(rend, BIRD, NULL, &pacdim);
+        else{
+            if(!started){
+                SDL_SetRenderDrawColor(rend, 100, 100, 100, 100);
+                SDL_Rect overlay = {0, 0, width, height};
+                SDL_RenderFillRect(rend, &overlay);
+                SDL_RenderCopy(rend, STRT, NULL, &strtrect);
+            }
+            else if(collided){
+                SDL_SetRenderDrawColor(rend, 100, 0, 0, 150);
+                SDL_Rect overlay = {0, 0, width, height};
+                SDL_RenderFillRect(rend, &overlay);
+                SDL_RenderCopy(rend, GOV, NULL, &govrect);
+            }
+            else{
+                SDL_SetRenderDrawColor(rend, 0, 0, 0, 150);
+                SDL_Rect overlay = {0, 0, width, height};
+                SDL_RenderFillRect(rend, &overlay);
+                SDL_RenderCopy(rend, PAUS, NULL, &pausrect);
+            }
+            // SDL_RenderCopy(rend, PAUS, NULL, NULL);
+
+        }
+        if(started){
+            SDL_RenderCopy(rend, score, NULL, &scorrect);
+            SDL_RenderCopy(rend, val, NULL, &valrect);
+        }
+        SDL_RenderCopy(rend, BIRD, NULL, &logorect);
+        
+
+
+        if(duration == 5){
+            duration = 0;
+            assinbrd.y = (frame/5)*168;
+            assinbrd.x = (frame%5)*183;
+            frame++;
+            if(frame == 14)
+                frame = 0;
+            duration = 0;
+        }
+        duration++;
+
+        SDL_RenderCopy(rend, BIRD_SPRITE, &assinbrd, &pacdim);
+        // SDL_RenderCopy(rend, BIRD, NULL, &pacdim);
         SDL_RenderPresent( rend );
         Uint32 frameDuration = SDL_GetTicks() - startTick;
         // std::cout << "Frame Duration: " << frameDuration << "ms" << std::endl;
@@ -223,23 +408,7 @@ int main(int argc, char* argv[])    {
         }
 
     }
-
-    if (BIRD != NULL) 
-        SDL_DestroyTexture(BIRD);
-    if (OBSTICLE != NULL) 
-        SDL_DestroyTexture(OBSTICLE);
-    if (REVOBSTICLE != NULL) 
-        SDL_DestroyTexture(REVOBSTICLE);
-    if (BIRD_SPRITE != NULL)
-        SDL_DestroyTexture(BIRD_SPRITE);
-
-
-
-    SDL_DestroyRenderer(rend);
-    SDL_DestroyWindow(win);
-    SDL_Quit();
-    IMG_Quit();
-
+    kill();
 
     return 0;
 
